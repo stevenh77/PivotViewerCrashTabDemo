d2f40218c2b864a1c2fda5d776933c10b8005538
Steven Hollidge <stevenhollidge@hotmail.com>
2013-02-08 23:30:24 +0100

    initial commit

---
+++ PivotViewerDemo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Pivot;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace PivotViewerDemo
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.pivotViewer.ItemsSource = Factory.GetData();
+        }
+    }
+}
---
+++ PivotViewerDemo/Shell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace PivotViewerDemo
+{
+    public partial class Shell
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
